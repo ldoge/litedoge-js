37a24d1c8e859de49706455e22e246ddc2e04031
Cody Droz <cody.droz@gmail.com>
2021-03-15 20:21:45 -0500

    Properly hash the genesis

--- scripts/gen.js
+++ scripts/gen.js
@@ -41,56 +41,60 @@ function createGenesisBlock(options) {
     time: options.time,
     bits: options.bits,
     nonce: options.nonce,
-    height: 0
+    height: 0,
+    vchBlockSig:  consensus.ZERO_HASH,
   });
 
   block.txs.push(tx);
   return block;
 }
 
+//hash: 0000032101032f27e7cdddb1196353f7fc9e1b6294717432135add95534f67c6
+//merkleRoot: 6e8089863e3811437cca6029c8eb113e0ddec4cb553e7cfdf8944c964cf86832
+//size: 171
 const main = createGenesisBlock({
   version: 1,
   time: 1426450258,
   bits: 504365055,
   nonce: 925125
 });
-
-const testnet = createGenesisBlock({
-  version: 1,
-  time: 1296688602,
-  bits: 486604799,
-  nonce: 414098458
-});
-
-const regtest = createGenesisBlock({
-  version: 1,
-  time: 1296688602,
-  bits: 545259519,
-  nonce: 2
-});
-
-const btcd = createGenesisBlock({
-  version: 1,
-  time: 1401292357,
-  bits: 545259519,
-  nonce: 2
-});
+//
+// const testnet = createGenesisBlock({
+//   version: 1,
+//   time: 1296688602,
+//   bits: 486604799,
+//   nonce: 414098458
+// });
+//
+// const regtest = createGenesisBlock({
+//   version: 1,
+//   time: 1296688602,
+//   bits: 545259519,
+//   nonce: 2
+// });
+//
+// const btcd = createGenesisBlock({
+//   version: 1,
+//   time: 1401292357,
+//   bits: 545259519,
+//   nonce: 2
+// });
 
 console.log(main);
 console.log('');
-console.log(testnet);
-console.log('');
-console.log(regtest);
-console.log('');
-console.log('');
+// console.log(testnet);
+// console.log('');
+// console.log(regtest);
+// console.log('');
+// console.log('');
 console.log('main hash: %s', main.rhash());
 console.log('main raw: %s', main.toRaw().toString('hex'));
 console.log('');
-console.log('testnet hash: %s', testnet.rhash());
-console.log('testnet raw: %s', testnet.toRaw().toString('hex'));
-console.log('');
-console.log('regtest hash: %s', regtest.rhash());
-console.log('regtest raw: %s', regtest.toRaw().toString('hex'));
-console.log('');
-console.log('btcd simnet hash: %s', btcd.rhash());
-console.log('btcd simnet raw: %s', btcd.toRaw().toString('hex'));
+// console.log('testnet hash: %s', testnet.rhash());
+// console.log('testnet raw: %s', testnet.toRaw().toString('hex'));
+// console.log('');
+// console.log('regtest hash: %s', regtest.rhash());
+// console.log('regtest raw: %s', regtest.toRaw().toString('hex'));
+// console.log('');
+// console.log('btcd simnet hash: %s', btcd.rhash());
+// console.log('btcd simnet raw: %s', btcd.toRaw().toString('hex'));
